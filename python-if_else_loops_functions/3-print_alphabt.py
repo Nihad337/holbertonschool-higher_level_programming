3fd39f7bc337cf5d1ada384c02a7459291998ae4
Nihad337 <nihadhmdov685@gmail.com>
2025-11-16 14:04:01 +0000

    asfasfas

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-for c in range(97, 123):  # ASCII 'a' (97) - 'z' (122)
-    if c != 101 and c != 113:  # e=101, q=113
+for c in range(97, 123):
+    if c != 101 and c != 113:
         print("{}".format(chr(c)), end="")
