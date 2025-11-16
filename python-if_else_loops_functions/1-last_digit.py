53369b7664a5aff8341db287cdcdd3d3b55dded4
Nihad337 <nihadhmdov685@gmail.com>
2025-11-16 13:57:38 +0000

    asfasf

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 
 number = random.randint(-10000, 10000)  # asfasf
 
-# Son rəqəmi doğru almaq üçün
+# Soasfasfasf
 last_digit = number % 10 if number >= 0 else number % -10
 
 if last_digit > 5:
@@ -11,4 +11,5 @@ if last_digit > 5:
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    msg = f"Last digit of {number} is {last_digit} and is less than 6 and not 0"
+print(msg)
