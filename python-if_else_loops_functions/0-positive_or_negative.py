a528ef344b55bb119a925d1718ea98fc108d9709
Nihad337 <nihadhmdov685@gmail.com>
2025-11-16 13:42:43 +0000

    asfasf

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10, 10)  # Bu hissəni dəyişməyin
+
+print(f"{number} is positive" if number > 0 else
+      f"{number} is zero" if number == 0 else
+      f"{number} is negative")
