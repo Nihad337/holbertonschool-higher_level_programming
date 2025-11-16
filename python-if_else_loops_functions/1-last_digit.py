4ebca8a3e087a4b7e9661011f6745edd330e2b44
Nihad337 <nihadhmdov685@gmail.com>
2025-11-16 14:00:48 +0000

    asfasf

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 import random
 
-number = random.randint(-10000, 10000)  #asfasf
+number = random.randint(-10000, 10000)  #asfas
 
-# asfasf
 last_digit = number % 10 if number >= 0 else number % -10
 
 if last_digit > 5:
