953179ab472e4634577b6f485f1b2127e8f202a9
Nihad337 <nihadhmdov685@gmail.com>
2025-11-16 13:52:12 +0000

    asfasf

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 import random
 
-number = random.randint(-10000, 10000)  # Dəyişdirməyin
+number = random.randint(-10000, 10000)  # asfsaf
 
-last_digit = number % 10  #asfasf
+last_digit = number - (number // 10) * 10  # asfasafssaf
+
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
-else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
