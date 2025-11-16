894512f257f1755a78de9289fcf3d3cef7acb09c
Nihad337 <nihadhmdov685@gmail.com>
2025-11-16 13:04:17 +0000

    asfasf

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(" ".join(s))
-print(str)
+str = str[39:67]+str[str.index("with"):str.index("with")+5]+""+str[:6]
+print(str[0])
