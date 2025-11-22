23d649f1e8f901e3df161b101d3ef4bf54813483
Nihad337 <nihadhmdov685@gmail.com>
2025-11-22 19:28:07 +0400

    asfasf

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    for number in my_list:
+        print("{:d}".format(number))#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    for number in my_list:
+        print("{:d}".format(number))
