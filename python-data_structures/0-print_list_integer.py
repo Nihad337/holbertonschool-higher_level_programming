dc407a06d8d1e9852ad900e42cdbc699602a9851
Nihad337 <nihadhmdov685@gmail.com>
2025-11-22 19:39:56 +0400

    asfa

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,8 +2,4 @@
 
 def print_list_integer(my_list=[]):
     for number in my_list:
-        print("{:d}".format(number))#!/usr/bin/python3
-
-def print_list_integer(my_list=[]):
-    for number in my_list:
         print("{:d}".format(number))
