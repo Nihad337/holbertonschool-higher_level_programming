0aceb5b86d086fdd33467afc6f5a8e05bf03cc51
Nihad337 <nihadhmdov685@gmail.com>
2025-11-22 18:09:27 +0400

    asfas

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,13 +2,15 @@
 from sys import argv
 
 if __name__ == "__main__":
-    argc = len(argv) - 1  # arqumentlərin sayı
+    argc = len(argv) - 1  # argv[0] is the script name
+
     if argc == 0:
         print("0 arguments.")
     else:
         if argc == 1:
             print("1 argument:")
         else:
-            print(f"{argc} arguments:")
-        for i, arg in enumerate(argv[1:], 1):
-            print(f"{i}: {arg}")
+            print("{} arguments:".format(argc))
+
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
