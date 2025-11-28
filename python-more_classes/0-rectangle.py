fe4bcc9ed118bbe65a79fa82e463a297f560099a
Nihad337 <nihadhmdov586@gmail.com>
2025-11-28 11:56:05 +0000

    asfas

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""
+This module defines an empty Rectangle class.
+"""
+
+
+class Rectangle:
+    """Empty class that defines a rectangle"""
+    pass#!/usr/bin/python3
+"""
+This module defines an empty Rectangle class.
+"""
+
+
+class Rectangle:
+        """Empty class that defines a rectangle"""
+            pass#!/usr/bin/python3
+        """
+        This module defines an empty Rectangle class.
+        """
+
+
+        class Rectangle:
+                """Empty class that defines a rectangle"""
+                    pass#!/usr/bin/python3
+                """
+                This module defines an empty Rectangle class.
+                """
+
+
+                class Rectangle:
+                        """Empty class that defines a rectangle"""
+                            pass#!/usr/bin/python3
+                        """
+                        This module defines an empty Rectangle class.
+                        """
+
+
+                        class Rectangle:
+                                """Empty class that defines a rectangle"""
+                                    pass#!/usr/bin/python3
+                                """
+                                This module defines an empty Rectangle class.
+                                """
+
+
+                                class Rectangle:
+                                        """Empty class that defines a rectangle"""
+                                            pass#!/usr/bin/python3
+                                        """
+                                        This module defines an empty Rectangle class.
+                                        """
+
+
+                                        class Rectangle:
+                                                """Empty class that defines a rectangle"""
+                                                    pass#!/usr/bin/python3
+                                                """
+                                                This module defines an empty Rectangle class.
+                                                """
+
+
+                                                class Rectangle:
+                                                        """Empty class that defines a rectangle"""
+                                                            pass#!/usr/bin/python3
+                                                        """
+                                                        This module defines an empty Rectangle class.
+                                                        """
+
+
+                                                        class Rectangle:
+                                                                """Empty class that defines a rectangle"""
+                                                                    pass
