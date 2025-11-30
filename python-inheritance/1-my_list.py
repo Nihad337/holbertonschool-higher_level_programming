a6aef4654f8603de7552f04def58d70962c34846
Nihad337 <nihadhmdov685@gmail.com>
2025-11-30 16:29:10 +0000

    asfasf

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-'''This is just a doc'''
-
+"""ojsdfhvifdyh"""
 
 
 class MyList(list):
-    '''Di yazdim da'''
-    
-    def __init__(self):
-        self.menimlistimdiyee = []
-    
-    def append(self, a):
-        self.menimlistimdiyee += [a]
-    
+    """fwjonfhyiyhi"""
+
     def print_sorted(self):
-        print(sorted(self.menimlistimdiyee))
-    
-    def __str__(self):
-        return str(self.menimlistimdiyee)
+        """fhgvyfdhgdfgbfdb"""
+        print(sorted(self))
