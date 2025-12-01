979b33ea878f403c61c019b089219653c10508c8
Nihad337 <nihadhmdov685@gmail.com>
2025-12-01 14:47:58 +0400

    asfasf

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""Fetches http://0.0.0.0:5050/status using urllib"""
 import urllib.request
 
-with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type {}". format(type(body)))
-    print("\t- content: {}". format(body))
-    print("\t- utf8 content: {}". format(body.decode('utf-8')))
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5050/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
